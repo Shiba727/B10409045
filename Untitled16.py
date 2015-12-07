78b559c54a3fbe6537be1a0aa56b922367e842ef
Shiba727 <a6513512@gmail.com>
2015-12-07 15:16:35 +0800

    台幣轉換器

---
+++ Untitled16.py
@@ -0,0 +1,54 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+#台幣轉換器
+while True:
+    a = input("請輸入想知道幣別：(按q退出)")
+    if a == "q":
+        break
+    if a=="日圓":
+        print("1 TWD= 3.75 JPY")
+    elif a=="美金":
+        print("1 TWD= 0.03 USD")
+    elif a=="歐元":
+        print("1 TWD= 0.03 EUR")
+    elif a=="澳幣":
+        print("1 TWD= 0.04 AUD")
+    elif a=="港幣":
+        print("1 TWD= 0.24 HKD")
+    elif a=="人民幣":
+        print("1 TWD= 0.20 CNY")
+    elif a=="英鎊":
+        print("1 TWD= 0.02 GBP")
+    elif a=="加拿大元":
+        print("1 TWD= 0.04 CAD")
+    elif a=="韓元":
+        print("1 TWD= 35.66 KRW")
+    elif a=="澳門元":
+        print("1 TWD= 0.24 MOP")
+    elif a=="盧布":
+        print("1 TWD= 2.07 RUB")
+    elif a=="馬爾地夫":
+        print("1 TWD= 0.47 MVR")
+    elif a=="埃及鎊":
+        print("1 TWD= 0.24 EGP")
+    elif a=="新加坡":
+        print("1 TWD= 0.04 SGD")
+    elif a=="馬來西亞":
+        print("1 TWD= 0.13 MYR")
+    elif a=="泰銖":
+        print("1 TWD= 1.09 THB")
+    elif a=="菲律賓":
+        print("1 TWD= 1.44 PHP")
+    elif a=="印尼盾":
+        print("1 TWD=423.25 IDR")
+    else:
+        print("本系統更新中---------請稍後")
+
+
+# In[ ]:
+
+
+
